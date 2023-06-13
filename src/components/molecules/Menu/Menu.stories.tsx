dcf7485540087a504b8654cf6c160547002da1dc
Jacob Peralta <Jadapema@hotmail.com>
2023-06-12 18:09:05 -0600

    feat: added Menu to molecules

---
+++ src/components/molecules/Menu/Menu.stories.tsx
@@ -0,0 +1,55 @@
+import React, {useState} from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import Menu from "./Menu";
+import {IconMenu} from "@tabler/icons";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+    title: "Watchit Molecules/ Menu",
+    component: Menu,
+} as ComponentMeta<typeof Menu>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Menu> = (args) => {
+  const [active, setActive] = useState('1')
+  const [isOpen, setIsOpen] = useState(true)
+  const handleToggleOpen = () => {
+    setIsOpen((open) => !open)
+  }
+  return <Menu {...args} onItemChange={setActive} active={active} isOpen={isOpen} onIsOpenChange={handleToggleOpen} />
+};
+
+export const ChannelItemTest = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+ChannelItemTest.args = {
+  items: [
+    {
+      id: '1',
+      icon: <IconMenu />,
+      title: 'Menu 1',
+      active: false,
+      onClick: () => console.log('clicked menu item')
+    },
+    {
+      id: '2',
+      icon: <IconMenu />,
+      title: 'Menu 2',
+      active: false,
+      onClick: () => console.log('clicked menu item')
+    },
+    {
+      id: '3',
+      icon: <IconMenu />,
+      title: 'Menu 3',
+      active: false,
+      onClick: () => console.log('clicked menu item')
+    },
+    {
+      id: '4',
+      icon: <IconMenu />,
+      title: 'Menu 4',
+      active: false,
+      onClick: () => console.log('clicked menu item')
+    }
+  ]
+}
---
+++ src/components/molecules/Menu/Menu.tsx
@@ -0,0 +1,60 @@
+import React, { FC } from "react";
+import { styled, Box, BoxProps } from "@mui/material";
+import { MenuItem, MenuItemProps } from "../../atoms/MenuItem";
+import Logo from "../../atoms/Logo";
+import Button from "../../atoms/Button";
+import {IconMenu} from "@tabler/icons";
+
+export type MenuProps = {
+  isOpen: boolean
+  items: MenuItemProps[]
+  active: string
+  onIsOpenChange?: () => void
+  onItemChange?: (id: string) => void
+}
+
+export const Menu: FC<MenuProps> = (props) : JSX.Element => {
+  return (
+      <MenuWrapper sx={{ backgroundColor: props.isOpen ? '#212328' : '#1A1C20' }}>
+        <MenuHeader>
+          <Logo size={100} />
+          <Button icon={<IconMenu />} variant={'flat'} backgroundColor={'transparent'} onClick={props.onIsOpenChange} />
+        </MenuHeader>
+        {
+          props.isOpen ? (
+            <>
+              {
+                props.items.map((el) => {
+                  return (
+                    <MenuItem {...el} active={el.id === props.active} onClick={props.onItemChange} />
+                  )
+                })
+              }
+            </>
+          ) : <></>
+        }
+      </MenuWrapper>
+  )
+}
+
+export const MenuWrapper = styled(Box)<BoxProps>(() => ({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'space-around',
+  width: '231px',
+  maxWidth: '231px',
+  padding: '0 1rem'
+}))
+
+export const MenuHeader = styled(Box)<BoxProps>(() => ({
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  height: '60px',
+  width: '100%',
+  padding: '0 1rem'
+}))
+
+export default Menu
