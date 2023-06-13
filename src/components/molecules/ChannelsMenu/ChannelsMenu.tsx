b0760d221d5d66a18437315189dde0ac5d6f00a0
Jacob Peralta <Jadapema@hotmail.com>
2023-06-12 18:08:39 -0600

    feat: added ChannelsMenu to molecules

---
+++ src/components/molecules/ChannelsMenu/ChannelsMenu.tsx
@@ -0,0 +1,35 @@
+import React, { FC } from "react";
+import { styled, Box, BoxProps, Typography } from "@mui/material";
+import Button from "../../atoms/Button";
+import { ChevronLeft, ChevronRight, Search } from "@mui/icons-material";
+
+export type MobileMenuProps = {
+  title: string,
+  onBack?: () => void
+  onForward?: () => void
+}
+
+export const ChannelsMenu: FC<MobileMenuProps> = (props) : JSX.Element => {
+  return (
+      <MobileHeaderWrapper>
+        <Box sx={{ display: 'flex' }}>
+          <Button variant={'flat'} icon={<ChevronLeft />} onClick={props.onBack} backgroundColor={'transparent'} />
+          <Button variant={'flat'} icon={<ChevronRight />} onClick={props.onForward} backgroundColor={'transparent'} />
+        </Box>
+        <Typography variant={'h4'} sx={{ color: '#D1D2D3' }}>{props.title}</Typography>
+        <Button variant={'flat'} icon={<Search />} onClick={props.onForward} backgroundColor={'transparent'} />
+      </MobileHeaderWrapper>
+  )
+}
+
+export const MobileHeaderWrapper = styled(Box)<BoxProps>(() => ({
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  height: '65px',
+  backgroundColor: '#1A1C20',
+  padding: '0 0.5rem'
+}))
+
+export default ChannelsMenu
